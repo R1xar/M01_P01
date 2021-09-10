c508dad26a2e166a30a40c53a03ee799f085c6f2
R1xar <ricardo.lopez.macedo@gmail.com>
2021-09-10 02:19:13 -0500

    Archivo raptor y su archivo cpp generado

---
+++ Raptor/P01_M01.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+int main()
+{
+   ?? salida;
+   ?? porta;
+   ?? time;
+   ?? ddra;
+
+   ddra =1;
+   while (1)
+   {
+      porta =1;
+      time =10;
+      while (1)
+      {
+         time =time-1;
+         if (time==0)) break;
+      }
+      porta =0;
+      time =10;
+      while (1)
+      {
+         time =time-1;
+         if (time==0)) break;
+      }
+      salida =false;
+      if (salida==true)) break;
+   }
+
+   return 0;
+}
